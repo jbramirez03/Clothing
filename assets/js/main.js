c1cfe0f6714c709472164d0944855f6bb8119236
temptado <jason1287712@gmail.com>
2022-12-28 13:58:21 -0500

    added script to show scroll up icon on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,14 @@ window.addEventListener('scroll', scrollActive);
 
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
 
 
 /*=============== DARK LIGHT THEME ===============*/
